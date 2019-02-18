35544fef04cf05fb5f6eb8c62a730fc0433bd6cb
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-18 18:34:27 +0100

    Finish task 25.5

--- server.js
+++ server.js
@@ -16,12 +16,35 @@ app.listen(PORT, function() {
 app.use(bodyParser.json());
 app.use(express.static('assets'));
 
+// Test middleware
+app.use((req, res, next) => {
+  console.log('Middleware test.');
+  next();
+});
+
 app.get('/', (req, res) => res.status(200).sendFile('assets/index.html'));
+
 app.post('/user-form', (req, res) => {
   const form = new formidable.IncomingForm();
   form.parse(req, (err, fields, files) => {
     if (err) throw err;
     console.log(fields);
-    res.status(202).end(JSON.stringify(fields));
+    res.status(202).json(fields);
   });
+});
+
+// Store middleware
+app.use('/store', (req, res, next) => {
+  console.log('Store middleware invoked.'.magenta);
+  next();
+});
+
+// GET for store endpoint
+app.get('/store', (req, res) => {
+  res.status(200).type('html').send('<h1>This is store</h1>');
+});
+
+app.use((req, res, next) => {
+  console.log('Resources not found.'.red);
+  res.sendStatus(404);
 });
