2104b8585ef7246b95627bc65a1d46f2f3c9514e
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-18 07:24:55 +0100

    Finish task 25.4

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+// const multiparty = require('multiparty');
+const formidable = require('formidable');
 require('colors');
 
 const PORT = 8080;
@@ -16,35 +18,13 @@ app.listen(PORT, function() {
 app.use(bodyParser.json());
 app.use(express.static('assets'));
 
-let stringifyFile;
-
 app.get('/', (req, res) => res.status(200).sendFile('assets/index.html'));
-app.get('/user-form', (req, res) => {
-  const form = {
-    'first-name': req.query['first-name'],
-    'last-name': req.query['last-name']
-  };
-  console.log(req.query);
-  res.status(202).end(JSON.stringify(form));
+app.post('/user-form', (req, res) => {
+  // const form = new multiparty.Form();
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) throw err;
+    console.log(fields);
+    res.status(202).end(JSON.stringify(fields));
+  });
 });
-
-app.get('/get-note', (req, res) => loadFile(res));
-app.post('/update-note/:note', (req, res) => updateFile(req, res));
-
-function loadFile(res) {
-  fs.readFile('./test.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    stringifyFile = data;
-    res.status(200).send(data);
-  });
-}
-
-function updateFile(req, res) {
-  stringifyFile = JSON.stringify(Object.assign(JSON.parse(stringifyFile), {value: req.params.note}));
-  fs.writeFile('./test.json', stringifyFile, err => {
-    if (err) throw err;
-    console.log('File updated.');
-    res.status(201).send(stringifyFile);
-  });
-}
-
