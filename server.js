b484147b55e05cd09a10ec0d101466ee5ec7a28d
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-20 08:14:14 +0100

    Refactor task solution

--- server.js
+++ server.js
@@ -7,27 +7,50 @@ const PORT = 8080;
 const app = express();
 app.use(bodyParser.json());
 
-let stringifyFile;
+const FILE_PATH = './test.json';
 
-function loadFile(res) {
-  fs.readFile('./test.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    stringifyFile = data;
+app.get('/get-note', (req, res) => {
+  fs.readFile(FILE_PATH, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
     res.status(200).send(data);
   });
-}
+});
 
-function updateFile(req, res) {
-  stringifyFile = JSON.stringify(Object.assign(JSON.parse(stringifyFile), {value: req.params.note}));
-  fs.writeFile('./test.json', stringifyFile, err => {
-    if (err) throw err;
-    console.log('File updated.');
-    res.status(201).send(stringifyFile);
+// Via appendFile
+app.post('/update-note-append/:chunk', (req, res) => {
+  const chunk = req.params.chunk;
+  fs.appendFile(FILE_PATH, chunk, err => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+    res.sendStatus(201);   
   });
-}
+});
 
-app.get('/get-note', (req, res) => loadFile(res));
-
-app.post('/update-note/:note', (req, res) => updateFile(req, res));
+// Via writeFile
+app.post('/update-note-write/:chunk', (req, res) => {
+  const chunk = {value: req.params.chunk};
+  fs.readFile(FILE_PATH, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+    console.log(JSON.parse(data));
+    console.log(chunk);
+    const updatedFile = Object.assign(JSON.parse(data), chunk);
+    console.log(updatedFile);
+    fs.writeFile(FILE_PATH, JSON.stringify(updatedFile), err => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+      }
+      res.sendStatus(201);   
+    });
+  });
+});
 
 app.listen(PORT, () => console.log(`Server listen on http://localhost:${PORT}`.blue));
